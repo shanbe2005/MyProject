eb9fcb460b98e0923dd3fbec0d9971a8d2ee69f1
shanmugam <shanmugam@adventsys.in>
2017-11-11 20:28:01 +0530

    First commit

---
+++ GitNetCore/GitNetCore/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace GitNetCore.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
---
+++ GitNetCore/GitNetCore/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace GitNetCore.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
